fc435392a2a43099251fe5e98c287e0ff1bb6db0
Brian T <bharveytan@gmail.com>
2014-04-12 00:08:09 -0700

    edited README, comments to controller.js, added angularjs CDN to index.html

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 minesense-codetest
 ==================
 
-5 day forecast
+Gives the 5 day weather forecast from Yahoo weather.
+
+Open the index.html file in a browser, and away we go...
---
+++ controller.js
@@ -0,0 +1,8 @@
+/**
+ * Controller.js for minesense-codetest app
+ * 
+ * TODO:
+ * - send contents of query.results.weather.rss.channel.item.forecast to scope
+ * - make request to Yahoo weather
+ * - monitor errors
+ */
