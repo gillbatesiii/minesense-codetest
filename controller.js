22dfc2e53aa749b1a2a1212ba4bd782cf8db8701
Brian T <bharveytan@gmail.com>
2014-04-12 02:40:13 -0700

    calling getForecasts() with ng-click

--- controller.js
+++ controller.js
@@ -10,6 +10,8 @@
 var weatherApp = angular.module('weatherApp', []);
 
 weatherApp.controller('ForecastController', function ($scope){
-	$scope.forecasts = [{"code":"29","date":"11 Apr 2014","day":"Fri","high":"64","low":"42","text":"Partly Cloudy"},{"code":"30","date":"12 Apr 2014","day":"Sat","high":"64","low":"41","text":"Partly Cloudy"},{"code":"32","date":"13 Apr 2014","day":"Sun","high":"69","low":"42","text":"Sunny"},{"code":"30","date":"14 Apr 2014","day":"Mon","high":"72","low":"44","text":"Partly Cloudy"},{"code":"11","date":"15 Apr 2014","day":"Tue","high":"59","low":"43","text":"Few Showers"}];
+	$scope.getForecasts = function(){
+	  $scope.forecasts = [{"code":"29","date":"11 Apr 2014","day":"Fri","high":"64","low":"42","text":"Partly Cloudy"},{"code":"30","date":"12 Apr 2014","day":"Sat","high":"64","low":"41","text":"Partly Cloudy"},{"code":"32","date":"13 Apr 2014","day":"Sun","high":"69","low":"42","text":"Sunny"},{"code":"30","date":"14 Apr 2014","day":"Mon","high":"72","low":"44","text":"Partly Cloudy"},{"code":"11","date":"15 Apr 2014","day":"Tue","high":"59","low":"43","text":"Few Showers"}];
+	}
 	
 })
