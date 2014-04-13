269be95368c2a9de51cb6587e1fc132c2d2b24a0
Brian T <bharveytan@gmail.com>
2014-04-13 01:55:31 -0700

    It works! (after a fashion..)
    When the page first loads, submitting without any input will result in an error
    
    TypeError: Cannot read property 'cityName' of undefined
        at g.$scope.getForecasts (http://localhost/minesense/controller.js:47:63)
    
    I *could* initialize $scope.query.cityName, but that would be cheating...
    
    probably
    
    still not handling http errors! when the service goes down, pop goes the weasel

--- controller.js
+++ controller.js
@@ -24,8 +24,18 @@ weatherApp.factory('forecastFactory', function($http){
 	}
 	var factory = {};
 	factory.getForecasts = function(cityName) {
-		ForecastResult.city = cityName || 'enter a city name';
-		ForecastResult.dailyForecasts = [{"code":"29","date":"11 Apr 2014","day":"Fri","high":"64","low":"42","text":"Partly Cloudy"},{"code":"30","date":"12 Apr 2014","day":"Sat","high":"64","low":"41","text":"Partly Cloudy"},{"code":"32","date":"13 Apr 2014","day":"Sun","high":"69","low":"42","text":"Sunny"},{"code":"30","date":"14 Apr 2014","day":"Mon","high":"72","low":"44","text":"Partly Cloudy"},{"code":"11","date":"15 Apr 2014","day":"Tue","high":"59","low":"43","text":"Few Showers"}];
+		cityName = cityName || 'Vancouver'; // defaults cityName to Vancouver if it's empty
+		
+		$http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.bylocation%20WHERE%20location%3D%22' + cityName + '%22&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=')
+		.success(function(data, status, headers, config){
+			console.log(data);
+			ForecastResult.dailyForecasts = data.query.results.weather.rss.channel.item.forecast;
+			ForecastResult.city = data.query.results.weather.rss.channel.location.city;
+		})
+		.error(function(data, status, headers, config){
+			//handle the error
+		})
+		// ForecastResult.dailyForecasts = [{"code":"29","date":"11 Apr 2014","day":"Fri","high":"64","low":"42","text":"Partly Cloudy"},{"code":"30","date":"12 Apr 2014","day":"Sat","high":"64","low":"41","text":"Partly Cloudy"},{"code":"32","date":"13 Apr 2014","day":"Sun","high":"69","low":"42","text":"Sunny"},{"code":"30","date":"14 Apr 2014","day":"Mon","high":"72","low":"44","text":"Partly Cloudy"},{"code":"11","date":"15 Apr 2014","day":"Tue","high":"59","low":"43","text":"Few Showers"}];
 		return ForecastResult;
 	}
 	
@@ -35,6 +45,7 @@ weatherApp.factory('forecastFactory', function($http){
 weatherApp.controller('ForecastController', function ($scope, forecastFactory){
 	$scope.getForecasts = function(){
 	  $scope.forecast = forecastFactory.getForecasts($scope.query.cityName);
+	 // $scope.$apply();
 	}
 	
 })
