6d1eb8dcc1d177797cfa2d270c2522d54f8f2465
Brian T <bharveytan@gmail.com>
2014-04-13 12:43:06 -0700

    updated comments

--- controller.js
+++ controller.js
@@ -2,9 +2,9 @@
  * Controller.js for minesense-codetest app
  * 
  * TODO:
- * - send contents of query.results.weather.rss.channel.item.forecast to scope
- * - make request to Yahoo weather
- * - monitor errors
+ * - send contents of query.results.weather.rss.channel.item.forecast to scope - done
+ * - make request to Yahoo weather - done
+ * - monitor errors 
  * 
  * 
  * 
@@ -13,9 +13,8 @@
 var weatherApp = angular.module('weatherApp', []);
 
 /**
- * factory: should query Yahoo weather, if request is successful 
- * check for errors in the response, 
- * then if no errors find the city and forecast and return to requesting entity
+ * factory: queries Yahoo weather passing cityName parameter 
+ * on success, find the city and forecast and return to requesting entity
  */
 weatherApp.factory('forecastFactory', function($http){
 	var ForecastResult = {
@@ -27,15 +26,20 @@ weatherApp.factory('forecastFactory', function($http){
 		cityName = cityName || 'Vancouver'; // defaults cityName to Vancouver if it's empty
 		
 		$http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.bylocation%20WHERE%20location%3D%22' + cityName + '%22&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=')
-		.success(function(data, status, headers, config){
-			console.log(data);
-			ForecastResult.dailyForecasts = data.query.results.weather.rss.channel.item.forecast;
-			ForecastResult.city = data.query.results.weather.rss.channel.location.city;
+		.success(function(data, status, headers, config){			
+			ForecastResult.dailyForecasts = data.query.results.weather.rss.channel.item.forecast || [];
+			ForecastResult.city = data.query.results.weather.rss.channel.location.city || 'city name not returned';
 		})
 		.error(function(data, status, headers, config){
 			//handle the error
+			console.log('data:');
+			console.log(data);
+			console.log('status:');
+			console.log(status);
+			console.log('headers:');
+			console.log(headers);
 		})
-		// ForecastResult.dailyForecasts = [{"code":"29","date":"11 Apr 2014","day":"Fri","high":"64","low":"42","text":"Partly Cloudy"},{"code":"30","date":"12 Apr 2014","day":"Sat","high":"64","low":"41","text":"Partly Cloudy"},{"code":"32","date":"13 Apr 2014","day":"Sun","high":"69","low":"42","text":"Sunny"},{"code":"30","date":"14 Apr 2014","day":"Mon","high":"72","low":"44","text":"Partly Cloudy"},{"code":"11","date":"15 Apr 2014","day":"Tue","high":"59","low":"43","text":"Few Showers"}];
+
 		return ForecastResult;
 	}
 	
@@ -45,7 +49,6 @@ weatherApp.factory('forecastFactory', function($http){
 weatherApp.controller('ForecastController', function ($scope, forecastFactory){
 	$scope.getForecasts = function(){
 	  $scope.forecast = forecastFactory.getForecasts($scope.query.cityName);
-	 // $scope.$apply();
 	}
 	
 })
